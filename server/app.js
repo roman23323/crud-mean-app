592ec1a6a6765c704ca6ca03250948d663687655
Roman <ignatenko_roman.s@mail.ru>
2025-12-02 14:08:31 +0300

    fix: client build output directory

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api/contacts', contactsRouter);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'client', 'public', 'index.html'));
 });
 
 // catch 404 and forward to error handler
